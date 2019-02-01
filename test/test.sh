8a94d8e8fcf9db1853e0145b0dab12b0162ac8e7
rch850 <rich850@gmail.com>
2019-02-01 17:14:07 +0900

    💚 Add test cases

---
+++ test/test.sh
@@ -0,0 +1,37 @@
+function git {
+  echo git $*
+}
+export -f git
+
+###################################################
+# lang: en
+###################################################
+
+diff -u <($(dirname $0)/../bin/git-review Request to merge FEATURE into TARGET) <(cat <<EOT
+git fetch origin
+git checkout origin/TARGET
+git merge --no-ff origin/FEATURE
+)
+
+diff -u <($(dirname $0)/../bin/git-review FEATURE) <(cat <<EOT
+git fetch origin
+git checkout origin/master
+git merge --no-ff origin/FEATURE
+)
+
+diff -u <($(dirname $0)/../bin/git-review FEATURE TARGET) <(cat <<EOT
+git fetch origin
+git checkout origin/TARGET
+git merge --no-ff origin/FEATURE
+)
+
+###################################################
+# lang: ja
+###################################################
+
+diff -u <($(dirname $0)/../bin/git-review マージをリクエスト FEATURE into TARGET) <(cat <<EOT
+git fetch origin
+git checkout origin/TARGET
+git merge --no-ff origin/FEATURE
+)
+
